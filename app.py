f3d32a7dd470b0d00bc79aca6cdb73a73fc69420
Md Rehan <mdrehan4all@gmail.com>
2022-10-18 19:41:58 +0530

    updated battery low percentage and added speaking speed

--- app.py
+++ app.py
@@ -11,21 +11,26 @@ def speak(text):
     engine.say(str(text))
     engine.runAndWait()
 
-speak("Hello World")
+speak("Hello World, I am your battery")
 
 def secs2hours(secs):
     mm, ss = divmod(secs, 60)
     hh, mm = divmod(mm, 60)
     return "%d:%02d:%02d" % (hh, mm, ss)
-    
+
+sleeptime = 60    
 while True:
     battery = psutil.sensors_battery()
     print(battery)
     print("charge = %s%%, time left = %s" % (battery.percent, secs2hours(battery.secsleft)))
-    if battery.percent <= 30 and battery.power_plugged == False:
+    if battery.percent <= 15 and battery.power_plugged == False:
         speak("Give me charge")
+        sleeptime = 10
+        
+    if battery.percent >= 25:
+        sleeptime = 60
     
     if battery.percent >= 100:
         speak("Battery Full")
         
-    time.sleep(10)
+    time.sleep(sleeptime)
